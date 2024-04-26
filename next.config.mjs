295c693cc728e9b2577806a36358160efc7038bd
Idan <holinikson@gmail.com>
2024-04-26 16:05:00 +0300

    Fixed Product Reel dont load, added Product Page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,34 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+// images:{
+//     remotePatterns:[
+//         {
+//             hostname:'localhost',
+//             pathname:'***',
+//             port:'3000',
+//             protocol:'http'
+//         },
+//     ],
+// }
+// };
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        // you can specify a particular path or use '*' for any path
+        pathname: '/**', // This will match all paths
+      },
+    ],
+  },
+};
 
 export default nextConfig;
