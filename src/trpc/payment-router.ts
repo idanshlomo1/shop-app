86820faac1e18f50b763b634fd2fc62a1153db86
Idan <holinikson@gmail.com>
2024-04-28 16:19:15 +0300

    orderId fix in url

--- src/trpc/payment-router.ts
+++ src/trpc/payment-router.ts
@@ -57,7 +57,7 @@ export const paymentRouter = router({
 
             try {
                 const stripeSession = await stripe.checkout.sessions.create({
-                    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId${order.id}`,
+                    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
                     cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/cart`,
                     payment_method_types: ["card", "paypal"],
                     mode: "payment",
