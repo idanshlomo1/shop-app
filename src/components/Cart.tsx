a62879e8554a764ade9d15d10e80fc2436be0206
Idan <holinikson@gmail.com>
2024-04-28 13:33:41 +0300

    Added Cart page

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import { useCart } from "@/hooks/use-cart";
 import { ScrollArea } from "./ui/scroll-area";
 import CartItem from "./CartItem";
+import { useEffect, useState } from "react";
 
 
 const Cart = () => {
@@ -16,6 +17,12 @@ const Cart = () => {
     const { items } = useCart()
 
     const itemCount = items.length;
+
+    const [isMounted, setIsMounted] = useState<boolean>(false)
+
+    useEffect(() => {
+        setIsMounted(true)
+    }, [])
 
     const cartTotal = items.reduce((total, { product }) => total + product.price, 0)
 
@@ -27,7 +34,7 @@ const Cart = () => {
                     aria-hidden="true"
                     className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500" />
                 <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
-                    {itemCount}
+                    {isMounted ? itemCount : 0}
                 </span>
             </SheetTrigger>
             <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
