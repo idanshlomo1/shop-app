140d939f33f21ecc5ceb36db76464427d29ce4da
Idan <holinikson@gmail.com>
2024-04-30 17:47:35 +0300

    update stripe api version

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,11 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-// images:{
-//     remotePatterns:[
-//         {
-//             hostname:'localhost',
-//             pathname:'***',
-//             port:'3000',
-//             protocol:'http'
-//         },
-//     ],
-// }
-// };
-
-// export default nextConfig;
-
-
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3000',
-        // you can specify a particular path or use '*' for any path
-        pathname: '/**', // This will match all paths
-      },
-    ],
+    domains: [
+      'localhost',
+      'shopfiles.netlify.app',
+    ]
   },
 };
 
--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,8 +1,16 @@
-import Stripe from "stripe"
+// import Stripe from "stripe"
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "",
-    {
-        apiVersion: '2024-04-10',
-        typescript: true,
-    }
-)
+// export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "",
+//     {
+//         apiVersion: '2024-04-10',
+//         typescript: true,
+//     }
+// )
+
+
+import Stripe from "stripe";
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
+    apiVersion: '2023-10-16',  // Ensure this matches the supported version in your type definitions
+    typescript: true,
+});
